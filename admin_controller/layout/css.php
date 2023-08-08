e9088675b75eee26d43cb4d4cb14547ba69b2e4c
Brian Thomas <56766194+brianbct129@users.noreply.github.com>
2023-08-08 17:49:53 +0800

    read_product

--- admin_controller/layout/css.php
+++ admin_controller/layout/css.php
@@ -7,3 +7,4 @@
     <!-- Custom styles for this template-->
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
     <link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../../../assets/css/fontello.css">
--- admin_controller/product_view.php
+++ admin_controller/product_view.php
@@ -38,7 +38,57 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
                 <!-- Begin Page Content -->
                 <div class="container-fluid">
 
-                    <!-- //tulis content -->
+                    <!-- Page Heading -->
+                    <h1 class="h3 mb-2 text-gray-800">Daftar Barang</h1>
+                    <p class="mb-4">DataTables is a third party plugin that is used to generate the demo table below.
+                        For more information about DataTables, please visit the <a target="_blank"
+                            href="https://datatables.net">official DataTables documentation</a>.</p>
+
+                    <!-- DataTales Example -->
+                    <div class="card shadow mb-4">
+                        <div class="card-header py-3">
+                            <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered text-nowrap" id="dataTable" width="100%" cellspacing="0">
+                                    <thead>
+                                        <tr>
+                                            <th>No.</th>
+                                            <th class="w-auto">Nama Barang</th>
+                                            <th>Harga</th>
+                                            <th>Keterangan</th>
+                                            <th width="10">Marketplace</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td class="align-middle text-center">1</td>
+                                            <td class="align-middle">
+                                                <div class="media">
+                                                    <img src="https://i.imgur.com/yQBJ68J.png" width="50" height="50" class="mr-3" alt="https://i.imgur.com/xmO8Lsp.jpg"> 
+                                                    <div class="media-body">
+                                                        <h6 class="mb-0">Gluco Dr</h6>
+                                                        <small class="category">
+                                                            <span class="badge badge-pill badge-primary">Gluco</span>
+                                                            <span class="badge badge-success badge-pill mr-1">Laboratory</span>
+                                                        </small>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                            <td class="align-middle text-center">Rp. 120.000</td>
+                                            <td class="text-wrap">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus harum, minus nihil expedita, et deserunt vel animi, ipsa adipisci labore eius iste sed distinctio temporibus. Hic a consequuntur quisquam placeat.</td>
+                                            <td class="align-middle text-center">
+                                                <button class="btn btn-primary btn-circle"><i class="ft-tokopedia"></i></button>
+                                                <button class="btn btn-primary btn-circle"><i class="ft-shopee"></i></button>
+                                                <button class="btn btn-primary btn-circle"><i class="ft-blibli"></i></button>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
                     
                 </div>
                 <!-- /.container-fluid -->
