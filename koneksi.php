f495784c8b2526a3bf598336e301999fa8add80b
Brian Thomas <56766194+brianbct129@users.noreply.github.com>
2023-08-28 18:17:42 +0800

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,6 @@
 <?php 
 
+// $koneksi = mysqli_connect("83.136.216.72","u1567541_agusvirga","V2e2Sy7E3s","u1567541_siagamedika");
 $koneksi = mysqli_connect("localhost","root","","siagamedika");
 
 if(!$koneksi){
