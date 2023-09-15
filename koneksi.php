381c10de1f9fd82f24ea1fa88809490e6a864ff1
Brian Thomas <56766194+brianbct129@users.noreply.github.com>
2023-09-15 18:45:34 +0800

    Update koneksi.php
    This reverts commit d7f24e41377a1b37c11cd4e6e0e76844d03bea87.

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,6 @@
 <?php 
 //Production Siagamedika
-$koneksi = mysqli_connect("194.163.42.23","u1567541_agusvirga","vst64yst90","u1567541_siagamedika");
+$koneksi = mysqli_connect("194.163.42.23","u9973847_siagamedika","S1i2A3g4A5","u9973847_siagamedika");
 
 //UAT Siagamedika
 // $koneksi = mysqli_connect("localhost","root","","siagamedika");
