064c3896bb90e3dd1909c5b8f28be6257cbc1a9c
Brian Thomas <56766194+brianbct129@users.noreply.github.com>
2023-09-15 19:45:20 +0800

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,11 +1,13 @@
-<?php 
-//Production Siagamedika
-$koneksi = mysqli_connect("194.163.42.23","u9973847_siagamedika","S1i2A3g4A5","u9973847_siagamedika");
+<?php
+// Production Siagamedika
+$koneksi = mysqli_connect("194.163.42.23", "u9973847_siagamedika", "S1i2A3g4A5", "u9973847_siagamedika");
 
-//UAT Siagamedika
-// $koneksi = mysqli_connect("localhost","root","","siagamedika");
+// UAT Siagamedika
+// $koneksi = mysqli_connect("localhost", "root", "", "siagamedika");
 
-if(!$koneksi){
-    die("Koneksi database gagal : ".mysqli_connect_error());
+if (!$koneksi) {
+    die("Koneksi database gagal: " . mysqli_connect_error());
+} else {
+    echo "Koneksi sudah oke"; // Tambahkan pesan ini jika koneksi berhasil
 }
-?> 
+?>
