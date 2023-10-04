cc39c4ae51a69115fd172b5e188620a10e50f324
Brian Thomas <56766194+brianbct129@users.noreply.github.com>
2023-10-04 22:30:22 +0800

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -2,7 +2,7 @@
 // Production Siagamedika
 // $koneksi = mysqli_connect("localhost", "u9973847_admin", "3SlK,tBxSHNI", "u9973847_siagamedika");
 
-$koneksi = mysqli_connect("83.136.216.72","u1567541_agusvirga","V2e2Sy7E3s","u1567541_siagamedika");
+$koneksi = mysqli_connect("localhost","u1567541_agusvirga","V2e2Sy7E3s","u1567541_siagamedika");
 
 // UAT Siagamedika
 // $koneksi = mysqli_connect("localhost", "root", "", "siagamedika");
